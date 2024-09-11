23c52d5e2494c1c868f9ae2210612844398bbedc
KusouUK <orion26br@gmail.com>
2024-09-11 10:20:18 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Quiz
+{
+	public class Program
+	{
+		public class Quiz
+		{
+			public string Question { get; set; }
+			public string[] Answers { get; set; }
+			public string CorrectAnswer {  get; set; }
+
+			public Quiz(string question, string[] answers, string correctAnswer)
+			{
+				Question = question;
+				Answers = answers;
+				CorrectAnswer = correctAnswer;
+			}
+		}
+
+		static void Main()
+		{
+			List<Quiz> question = new List<Quiz>();
+			question.AddRange(Questions.GetQuestions());
+
+			int randomIndex = new Random().Next(0, question.Count);
+
+			Console.WriteLine(question[randomIndex].Question + "\n");
+
+			char[] options = ['A', 'B', 'C', 'D'];
+			for (int i = 0; i < question[randomIndex].Answers.Length; i++)
+			{
+				Console.WriteLine($"{options[i]}. {question[randomIndex].Answers[i]}");
+			}
+			Console.Write("\nAnswer: ");
+			string userAnswer = Console.ReadLine()!.ToLower();
+
+			bool isValidAnswer = userAnswer.All<char>(c => "abcd".Contains(c));
+			if (userAnswer.Length <= 0 || userAnswer.Length >= 2 || !isValidAnswer)
+			{
+				Console.WriteLine("Invalid answer");
+				Environment.Exit(0);
+			}
+
+			switch (userAnswer) 
+			{
+				case "a":
+					userAnswer = question[randomIndex].Answers[0];
+					break;
+				case "b":
+					userAnswer = question[randomIndex].Answers[1];
+					break;
+				case "c":
+					userAnswer = question[randomIndex].Answers[2];
+					break;
+				case "d":
+					userAnswer = question[randomIndex].Answers[3];
+					break;
+				default:
+					Console.WriteLine("Invalid somehow.");
+					break;
+			}
+
+			if(userAnswer != question[randomIndex].CorrectAnswer)
+			{
+				Console.WriteLine($"Wrong! The correct answer was: {options[Array.FindIndex(question[randomIndex].Answers, w => w == question[randomIndex].CorrectAnswer)]}. {question[randomIndex].CorrectAnswer}");
+				Environment.Exit(0);
+			}
+
+			Console.WriteLine("You are correct!");
+		}
+	}
+}
---
+++ Questions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+	public class Questions
+	{
+		public static List<Program.Quiz> GetQuestions()
+		{
+			List<Program.Quiz> question = new List<Program.Quiz>();
+			question.Add(new Program.Quiz("Who is Bjarne Stroustrup?", ["The creator of Python", "The creator of Java", "The creator of C#", "The creator of C++"], "The creator of C++"));
+			question.Add(new Program.Quiz("Quem é doc?", ["Gênio do python", "Vagabundo", "doido", "lindo"], "doido"));
+			return question;
+		}
+	}
+}
