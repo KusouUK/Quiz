47960bdb520830d5eda744a23bc2219fd059457d
KusouUK <orion26br@gmail.com>
2024-09-14 18:06:19 -0300

    .

--- Questions.cs
+++ Questions.cs
@@ -4,13 +4,15 @@
 	{
 		public static List<Program.Quiz> GetQuestions()
 		{
-			List<Program.Quiz> question = new List<Program.Quiz>();
-			question.Add(new Program.Quiz("Who is Bjarne Stroustrup?", ["The creator of Python", "The creator of Java", "The creator of C#", "The creator of C++"], "The creator of C++"));
-			question.Add(new Program.Quiz("Quem é doc?", ["Gênio do python", "Vagabundo", "doido", "lindo"], "doido"));
-			question.Add(new Program.Quiz("True or false?", ["true", "false"], "true"));
-			question.Add(new Program.Quiz("Which number is the greatest?", ["1", "2", "3", "4", "5"], "5"));
+			List<Program.Quiz> questions = new List<Program.Quiz>()
+			{
+				{ new Program.Quiz("Who is Bjarne Stristrup", ["The creator of Python", "The creator of Java", "The creator of C#", "The creator of C++"], "The creator of C++") },
+				{ new Program.Quiz("Quem é doc?", ["Gênio do python", "Vagabundo", "doido", "lindo"], "doido") },
+				{ new Program.Quiz("True or false?", ["true", "false"], "true") },
+				{ new Program.Quiz("Which number is the greatest?", ["1", "2", "3", "4", "5"], "5") }
+			};
 
-			return question;
+			return questions;
 		}
 	}
 }
